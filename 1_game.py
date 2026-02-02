ce404ee2a76fa462723b838270c9858f2bcf4917
Elwing-Chou <birdfan8814@gmail.com>
2026-02-02 13:53:36 +0800

    Add files via upload

--- 1_game.py
+++ 1_game.py
@@ -6,5 +6,10 @@ import random
 my = int(input("可以輸入 0:剪刀 1:石頭 2:布"))
 com = random.randint(0, 2)
 
-print(my)
-print(com)
+# 轉換
+# list(清單): l = [xxx, ooo, ddd]
+#           編號    0    1    2
+# 操作(查詢): l[編號]
+trans = ["剪刀", "石頭", "布"]
+print("我:" + trans[my])
+print("電腦:" + trans[com])
