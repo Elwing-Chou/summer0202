cb6727e06a5fe6320af2ac5770e0c6e3dc8328fc
Elwing-Chou <birdfan8814@gmail.com>
2026-02-02 11:55:00 +0800

    Add files via upload

---
+++ 1_game.py
@@ -0,0 +1,10 @@
+import random
+# 功能: 1. (參數) 2. 名字 = 回傳答案
+# 型態(分類): 1. 數字(int/float) 2.字串("")(str)
+# 型態轉換: int() float() str()
+# 的: .
+my = int(input("可以輸入 0:剪刀 1:石頭 2:布"))
+com = random.randint(0, 2)
+
+print(my)
+print(com)
