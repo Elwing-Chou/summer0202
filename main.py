4861f354045cfdff8442e00be677888d961f10a7
Elwing-Chou <birdfan8814@gmail.com>
2026-02-02 11:03:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ print("哈囉")
 # 代號(=)
 # 5 == 3 + 2(相等)
 # x = 3(取代)
-height = 150
+height = 180
 print(height)
 print(height/100)
+
+# 縮排(強調): 冒號和TAB
+# 如果(if) 否則(else)
+if height > 175:
+    print("很高")
+else:
+    print("還好")
