4814b18878fd2fdc93233a9fa058ccc9a9c397c5
Elwing-Chou <birdfan8814@gmail.com>
2026-02-03 10:19:22 +0800

    Add files via upload

--- 3_draw.py
+++ 3_draw.py
@@ -3,7 +3,7 @@ import random
 
 turtle.shape("turtle")
 turtle.colormode(255)
-turtle.speed(3)
+turtle.speed(0)
 
 radius = 150
 turtle.penup()
@@ -11,12 +11,15 @@ turtle.forward(radius)
 turtle.pendown()
 turtle.left(90)
 
-r = random.randint(0,255)
-g = random.randint(0,255)
-b = random.randint(0,255)
-turtle.fillcolor(r,g,b)
-turtle.begin_fill()
-turtle.circle(radius, 180)
-turtle.end_fill()
+i = 0
+while i < 100:
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    turtle.fillcolor(r,g,b)
+    turtle.begin_fill()
+    turtle.circle(radius-1*i, 180)
+    turtle.end_fill()
+    i = i + 1
 
 turtle.done()
