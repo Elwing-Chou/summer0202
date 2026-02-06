8a9a32ea97717942cfdab286f6d52469fbebb167
Elwing-Chou <birdfan8814@gmail.com>
2026-02-06 11:36:33 +0800

    Add files via upload

--- 8_動畫瘋.py
+++ 8_動畫瘋.py
@@ -16,25 +16,30 @@ for a in html.find_all("a"):
     href = a["href"]
     if href.startswith("?sn"):
         fullurl = "https://ani.gamer.com.tw/animeVideo.php" + href
-        print(fullurl)
+        # print(fullurl)
+        serial = href.split("=")[1]
+        # 拿第幾集
+        chapter = a.text
+        print(chapter)
 
-# 下載一集的danmu
-name = "baha/吉伊卡哇"
-# 如果資料夾不存在, make起來
-if not os.path.exists(name):
-    os.makedirs(name)
+        # 下載一集的danmu
+        name = "baha/吉伊卡哇"
+        # 如果資料夾不存在, make起來
+        if not os.path.exists(name):
+            os.makedirs(name)
 
-url = "https://api.gamer.com.tw/anime/v1/danmu.php?videoSn=36632&geo=TW%2CHK"
-resp = req.urlopen(url)
-resp_l = json.load(resp)
-danmu = resp_l["data"]["danmu"]
+        url = "https://api.gamer.com.tw/anime/v1/danmu.php?videoSn=" + serial + "&geo=TW%2CHK"
+        resp = req.urlopen(url)
+        resp_l = json.load(resp)
+        danmu = resp_l["data"]["danmu"]
 
-sn = url.split("?")[1].split("&")[0].split("=")[1]
-# CSV(comma-separated values)
-# 姓名,身高
-# Elwing,175
-# DataFrame: pandas表格
-df = pd.DataFrame(danmu)
-fp = name + "/" + sn + ".csv"
-df.to_csv(fp)
-# df.to_excel("chiikawa.xlsx")
+        sn = url.split("?")[1].split("&")[0].split("=")[1]
+        # CSV(comma-separated values)
+        # 姓名,身高
+        # Elwing,175
+        # DataFrame: pandas表格
+        df = pd.DataFrame(danmu)
+        df["chapter"] = chapter
+        fp = name + "/" + sn + ".csv"
+        df.to_csv(fp)
+        # df.to_excel("chiikawa.xlsx")
